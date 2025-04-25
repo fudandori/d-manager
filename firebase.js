655f6f09e68ea32dfaa7a2d7c9e955def025bd65
fudandori <fudandori@gmail.com>
2025-04-25 16:17:33 +0200

    wip

--- firebase.js
+++ firebase.js
@@ -5,7 +5,9 @@ import { getAuth, signInWithEmailAndPassword } from 'https://www.gstatic.com/fir
 import { getDatabase, ref, child, get, push } from 'https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js'
 
 const LEDGER = 'ledger'
+const GIRLS = 'girls'
 const LEDGER_FORM = 'ledger-form-container'
+const GIRLS_FORM = 'girls-form-container'
 
 let dbRef = null
 let canvas = null
@@ -38,7 +40,9 @@ const init = () => {
     document.getElementById('button2').addEventListener('click', girls)
     document.getElementById('button3').addEventListener('click', me)
     document.getElementById('cancel-ledger').addEventListener('click', () => unpop(LEDGER_FORM))
+    document.getElementById('cancel-girls').addEventListener('click', () => unpop(GIRLS_FORM))
     document.getElementById('send-ledger').addEventListener('click', () => readLedgerForm())
+    document.getElementById('send-girls').addEventListener('click', () => readGirlsForm())
     //document.getElementById('button4').addEventListener('click', writeLedger)
 }
 
@@ -54,11 +58,7 @@ const ledger = () => {
 
             canvas.innerHTML = ''
 
-            const addButton = document.createElement('button')
-
-            addButton.innerHTML = '<i class="material-icons" style="font-size:3em;">add_box</i>'
-            addButton.classList.add('add-button')
-            addButton.addEventListener('click', editLedger)
+            const addButton = renderAddButton(LEDGER_FORM)
 
             const grid = document.createElement('div')
 
@@ -67,8 +67,7 @@ const ledger = () => {
             let acc = 0
             Object.values(window.myAux).forEach((o, i) => {
                 acc += o.in - o.out
-                const date = formatDate(o.time)
-                insertLedgerGrid(grid, acc, o.out, o.in, o.concept, date)
+                insertLedgerGrid(grid, acc, o.out, o.in, o.concept, o.time)
             })
 
             spawnHeader(grid, 'money', 'arrowred', 'arrowdown', 'pen', 'calendar')
@@ -81,15 +80,29 @@ const ledger = () => {
 }
 
 const girls = () => {
-    get(child(dbRef, 'girls'))
+    get(child(dbRef, GIRLS))
         .then((snapshot) => {
             if (!snapshot.exists()) {
                 console.log('No data available')
                 return
             }
 
-            document.getElementById('canvas').innerHTML = JSON.stringify(snapshot.val())
+            window.myAux = snapshot.val()
 
+            canvas.innerHTML = ''
+
+            const addButton = renderAddButton(GIRLS_FORM)
+
+            const grid = document.createElement('div')
+            grid.classList.add('grid', 'girls')
+
+            insertGrid(grid, 'guest', 'name', 'pay', 'hours', 'date')
+
+            spawnHeader(grid, 'user', 'girl', 'money', 'hourglass', 'calendar')
+
+
+            canvas.appendChild(addButton)
+            canvas.appendChild(grid)
 
         })
         .catch((error) => console.error(error))
@@ -112,25 +125,34 @@ const me = () => {
 
 }
 
+const readGirlsForm = () => {
+    const name = document.getElementById('name').value;
+    const pay = document.getElementById('pay').value;
+    const time = document.getElementById('time').value;
+    const guest = document.getElementById('guest').value;
+
+    writeGirls(name, pay, time, guest)
+}
+
 const readLedgerForm = () => {
     const concept = document.getElementById('concept').value;
     const inValue = document.getElementById('in').value;
     const outValue = document.getElementById('out').value;
 
     writeLedger(concept, inValue, outValue)
-};
+}
 
 const unpop = (id) => {
     document.getElementById(id).classList.remove('visible')
 }
 
-const editLedger = () => {
-    document.getElementById(LEDGER_FORM).classList.add('visible')
+const editBox = (box) => {
+    document.getElementById(box).classList.add('visible')
 }
 
 const writeLedger = (concept, valueIn, valueOut) => {
     push(child(dbRef, LEDGER), {
-        time: new Date().toISOString(),
+        time: getDate(),
         concept: concept,
         in: valueIn,
         out: valueOut
@@ -143,8 +165,24 @@ const writeLedger = (concept, valueIn, valueOut) => {
 
 }
 
-const formatDate = (str) => {
-    const date = new Date(str);
+const writeGirls = (name, pay, time, guest) => {
+    push(child(dbRef, GIRLS), {
+        date: getDate(),
+        name: name,
+        hours: time,
+        pay: pay,
+        guest: guest
+    })
+
+    setTimeout(() => {
+        girls()
+        setTimeout(() => unpop(GIRLS_FORM), 500)
+    }, 500)
+
+}
+
+const getDate = () => {
+    const date = new Date()
 
     const d = String(date.getDate()).padStart(2, '0');
     const m = String(date.getMonth() + 1).padStart(2, '0');
@@ -154,8 +192,10 @@ const formatDate = (str) => {
 };
 
 const insertGrid = (grid, ...str) => {
-    str.forEach((text) => grid.appendChild(Object.assign(document.createElement('span'), { textContent: text })))
+    Object.values(window.myAux).forEach(o =>
+        str.forEach((text) => grid.prepend(Object.assign(document.createElement('span'), { textContent: o[text] }))))
 }
+
 
 const spawnHeader = (grid, ...headers) => {
 
@@ -178,6 +218,16 @@ const insertLedgerGrid = (grid, ...str) => {
         if (i % 4 === 0 && v < 0) span.style.backgroundColor = 'darkred'
         grid.prepend(span)
     })
+}
+
+const renderAddButton = (box) => {
+    const addButton = document.createElement('button')
+
+    addButton.innerHTML = '<i class="material-icons" style="font-size:3em;">add_box</i>'
+    addButton.classList.add('add-button')
+    addButton.addEventListener('click', () => editBox(box))
+
+    return addButton
 }
 
 init()
